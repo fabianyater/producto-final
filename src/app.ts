bf1c4fa591976f112c835d4cef1e3ab6fff69157
Fabian Yate <fabianyate31@gmail.com>
2024-11-19 06:32:16 -0500

    .

--- src/app.ts
+++ src/app.ts
@@ -31,6 +31,11 @@ app.use((req: Request, res: Response, next: NextFunction) => {
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.setHeader("Access-Control-Allow-Credentials", "true");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+
   next();
 });
 
