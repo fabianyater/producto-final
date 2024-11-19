717484e31f59eef7991a5b4883784525a071b9a4
Fabian Yate <fabianyate31@gmail.com>
2024-11-18 21:44:46 -0500

    add preflight cors

--- src/app.ts
+++ src/app.ts
@@ -21,14 +21,7 @@ app.use(
   })
 );
 
-app.options("*", (req, res) => {
-  res.header("Access-Control-Allow-Origin", "https://bitacora-web-blue.vercel.app");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type,Authorization,x-access-token");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.sendStatus(200); // Responde con un código 200
-});
-
+app.options("*", cors());
 
 connectDB();
 
